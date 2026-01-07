7f32a525a1adc646706588c75e5ffb30adc8d4ed
prajwalwankhade143 <prajwalwankhade202@gmail.com>
2026-01-07 21:35:25 +0530

    test deploy

--- app.py
+++ app.py
@@ -1,28 +1,5 @@
 import streamlit as st
-from auth import register, login
 
-st.set_page_config(page_title="NextChapter")
-
-st.title("NextChapter")
-
-choice = st.sidebar.selectbox("Menu", ["Register", "Login"])
-
-if choice == "Register":
-    name = st.text_input("Name")
-    email = st.text_input("Email")
-    password = st.text_input("Password", type="password")
-
-    if st.button("Register"):
-        register(name, email, password)
-        st.success("Registered successfully")
-
-if choice == "Login":
-    email = st.text_input("Email")
-    password = st.text_input("Password", type="password")
-
-    if st.button("Login"):
-        user = login(email, password)
-        if user:
-            st.success("Login successful")
-        else:
-            st.error("Invalid credentials")
+st.title("🔥 NEXTCHAPTER TEST APP")
+st.success("Agar ye dikh raha hai to deploy SUCCESS hai")
+st.write("Time:", st.time_input("now"))
